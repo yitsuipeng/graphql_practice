ddaef1d544f65f0290fffdf6522661c0a8597088
yitsuipeng <erin.peng@aiello.ai>
2022-06-23 19:24:48 +0800

    add docker compose yml

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.get("/", (req, res) => {
 
 // Page not found
 app.use(function(req, res, next) {
-  res.status(404).sendFile(__dirname + '/public/404.html');
+  res.status(404).send('Not found');
 });
 
 // Error handling
